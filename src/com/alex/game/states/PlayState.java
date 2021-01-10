332a92c03ffcc07aee49fe04e969125acf80a2a1
Alex Cruz <alexcruz182@gmail.com>
2021-01-10 18:02:54 -0500

    ep 7

--- src/com/alex/game/states/PlayState.java
+++ src/com/alex/game/states/PlayState.java
@@ -1,14 +1,18 @@
 package com.alex.game.states;
 
+import com.alex.game.graphics.Font;
+import com.alex.game.graphics.Sprite;
 import com.alex.game.util.KeyHandler;
 import com.alex.game.util.MouseHandler;
+import com.alex.game.util.Vector2f;
 
 import java.awt.*;
 
 public class PlayState extends GameState{
-
+    private Font font;
     public PlayState(GameStateManager gms) {
         super(gms);
+        font = new Font("font/ZeldaFont.png",16,16);
     }
 
     @Override
@@ -25,7 +29,6 @@ public class PlayState extends GameState{
 
     @Override
     public void render(Graphics2D g) {
-        g.setColor(Color.RED);
-        g.fillRect(100,200,64,64);
+        Sprite.drawArray(g,font, "a your mom", new Vector2f(100,100), 32 , 32,32 ,0);
     }
 }
